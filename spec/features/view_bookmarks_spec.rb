855a60e3256d4b77333418236d5b7c74abfbcf2e
Juel07 <juel.braganza97@gmail.com>
2021-06-22 16:26:27 +0100

    created insertion/truncation test cycle for test db

--- spec/features/view_bookmarks_spec.rb
+++ spec/features/view_bookmarks_spec.rb
@@ -1,9 +1,17 @@
+require "pg"
+
 feature "Viewing bookmarks" do
   scenario "seeing bookmarks" do
     visit("/bookmarks")
     expect(page).to have_content "Bookmarks:"
   end
   scenario "seeing list of bookmarks" do
+    connection = PG.connect(dbname: "bookmark_manager_test")
+
+    connection.exec("INSERT INTO bookmarks (url, name) VALUES ('http://www.twitter.com/', 'Twitter');")
+    connection.exec("INSERT INTO bookmarks (url, name) VALUES ('http://www.makers.tech/', 'Makers');")
+    connection.exec("INSERT INTO bookmarks (url, name) VALUES ('http://www.askjeeves.com/', 'AskJeeves');")
+
     visit("/bookmarks")
     expect(page).to have_content "Makers: http://www.makers.tech/"
     expect(page).to have_content "Twitter: http://www.twitter.com"
