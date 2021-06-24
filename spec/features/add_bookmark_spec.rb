748bb3dabbbb8a25b996202bd0d895e5feb5c5c1
Juel07 <juel.braganza97@gmail.com>
2021-06-24 14:13:59 +0100

    add styles

--- spec/features/add_bookmark_spec.rb
+++ spec/features/add_bookmark_spec.rb
@@ -4,10 +4,10 @@ feature "a user can add new bookmarks" do
 
   scenario "a user adds a bookmark" do
     visit("/bookmarks")
-    click_link("Add bookmark")
+    click_button("Add bookmark")
     fill_in(:name, with: name)
     fill_in(:url, with: url)
-    click_button("submit")
+    click_button("Submit")
     expect(page).to have_link("#{name}", href: "#{url}")
   end
 end
--- spec/features/view_bookmarks_spec.rb
+++ spec/features/view_bookmarks_spec.rb
@@ -3,7 +3,7 @@ require "pg"
 feature "Viewing bookmarks" do
   scenario "seeing bookmarks" do
     visit("/bookmarks")
-    expect(page).to have_content "Bookmarks:"
+    expect(page).to have_content "Bookmarks"
   end
 
   scenario "seeing list of bookmarks" do
